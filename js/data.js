6498abdc23b6d4df9c3739dddf8eaabd5dfda3b3
Niels Dommerholt <ndommerholt@bol.com>
2020-03-24 09:56:05 +0100

    Separated data from main.js

---
+++ js/data.js
@@ -0,0 +1,6 @@
+const speurtocht = [
+    { number: 1, hint: "Ga de voordeur uit en volg de RODE pijlen"},
+    { number: 2, hint: "Welke kleur heeft sneeuw? Dit is de kleur van de pijlen die je moet volgen."},
+    { number: 3, hint: "Kijk goed om je heen of je de volgende hint ziet!"},
+    { number: 4, hint: "Test 4"},
+];
--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-const speurtocht = [
-    { number: 1, hint: "Ga de voordeur uit en volg de RODE pijlen"},
-    { number: 2, hint: "Welke kleur heeft sneeuw? Dit is de kleur van de pijlen die je moet volgen."},
-    { number: 3, hint: "Kijk goed om je heen of je de volgende hint ziet!"},
-    { number: 4, hint: "Test 4"},
-];
-
 speurtocht.forEach(e => {
     const div_id = "question_" + e.number;
     const qr_id = "qr_" + e.number;
