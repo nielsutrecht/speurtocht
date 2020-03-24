ea42e3cf516e37cdff3c7575251a95d1570ff461
Niels Dommerholt <ndommerholt@gmail.com>
2020-03-24 22:09:17 +0100

    Update

--- js/data.js
+++ js/data.js
@@ -1,6 +1,16 @@
 const speurtocht = [
-    { number: 1, hint: "Ga de voordeur uit en volg de RODE pijlen"},
-    { number: 2, hint: "Welke kleur heeft sneeuw? Dit is de kleur van de pijlen die je moet volgen."},
-    { number: 3, hint: "Kijk goed om je heen of je de volgende hint ziet!"},
-    { number: 4, hint: "Test 4"},
+    { number: 1, hint: "Ga de voordeur uit en volg de ROZE pijlen!"},
+    { number: 2, hint: "Hallo! Lekker bezig! Volg nu de BLAUWE pijlen!"},
+    { number: 3, hint: "Volg de GROENE pijlen!"},
+    { number: 4, hint: "Steek de weg over en volg de GELE pijlen!"},
+    { number: 5, hint: "Je krijgt van Fleur opdrachten om uit te voeren!"},
+    { number: 6, hint: "Volg het pad naar het bankje naast de prullenbak"},
+    { number: 7, hint: "Volg het pad naar weer een bankje naast een prullenbak"},
+    { number: 8, hint: "Vind een bord met een dakje erboven"},
+    { number: 9, hint: "Je krijgt van Fleur weer opdrachten om uit te voeren!"},
+    { number: 10, hint: "Volg de pijlen op de bomen tot aan de poort"},
+    { number: 11, hint: "Het eerste bankje na de brug"},
+    { number: 12, hint: "Volg de weg naar een bankje meteen naast een gele prullenbak"},
+    { number: 13, hint: "Zoek het hek!"},
+    { number: 14, hint: "https://nielsutrecht.github.io/speurtocht/20200325zdjg.html"}
 ];
